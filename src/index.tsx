7222795910f68ae3a4d6d39843c36aa61ae90ac9
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-08-17 15:48:51 +0200

    Basic counter implemented within class component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface AppProps {
+    color: string;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface AppState {
+    counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+    //state = { counter: 0 }  # we define state of App class we are not override state in React.Component
+
+    constructor(props: AppProps){
+        super(props)
+        //overriding state in Component class
+        this.state = {counter: 0}
+    }
+
+    onIncrement = (): void => {
+        this.setState((prevState: AppState)=>({ counter: prevState.counter + 1 }))
+    }
+
+    onDecrement = (): void => {
+        this.setState((prevState: AppState)=>({ counter: prevState.counter -1 }))
+    }
+
+    public render() {
+        return(
+            <div>
+                {this.state.counter}
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App color="red"/>, document.querySelector("#root"))
